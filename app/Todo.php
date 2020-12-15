a0bb12d5903abc9ff82b6fd3412130987c249001
morihisashi <hisa.1995.322@outlook.com>
2020-12-15 12:33:02 +0000

    impl relation

--- app/Todo.php
+++ app/Todo.php
@@ -7,6 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 class Todo extends Model
 {
     use \Rutorika\Sortable\SortableTrait;
+    
     public function user()
     {
         return $this->belongsTo('App\User');
